78d8b7a2cf46da63d81031ff41f51e2b9a046555
tquangdang <thanhquangdang2002@gmail.com>
2024-04-28 15:51:09 -0500

    Update views.py

--- inventory/views.py
+++ inventory/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from store.models import Product, Supplier, Buyer, Order
 
-# This view function displays the dashboard page, showing counts and lists of products, suppliers, buyers, and orders.
-# The view is decorated with `login_required` to ensure only authenticated users can access this page.
-# The `login_url='login'` argument redirects unauthenticated users to the login page.
 @login_required(login_url='login')
 def dashboard(request):
     # Retrieve counts of all products, suppliers, buyers, and orders from the database.
@@ -13,7 +10,7 @@ def dashboard(request):
     total_buyer = Buyer.objects.count()
     total_oder = Order.objects.count()
     
-    # Retrieve all orders and order them by most recent.
+    # Retrieve all orders 
     orders = Order.objects.all().order_by('-id')
     
     # Context dictionary to pass the counts and orders to the template.
